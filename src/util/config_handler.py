fbe3607cc8162de5aa0f3dcd005eed603931916f
igarashi339 <igarashi339@gmail.com>
2020-08-29 12:55:23 +0900

    パスを取得するメソッドを修正

--- src/util/config_handler.py
+++ src/util/config_handler.py
@@ -5,45 +5,37 @@ class ConfigHandler:
     とりあえずローカルのパスをべた書きしている...
     '''
     def __init__(self):
-        self.local_root = "C:/Users/Yu IGARASHI/PycharmProjects/maihama_bot/"
+        self.local_roots = [
+            "C:/Users/Yu IGARASHI/PycharmProjects/maihama_bot/",
+            ""
+        ]
         self.lolipop_root = "/home/users/0/her.jp-everyday-micmin/web/maihama_bot/"
 
     def get_api_key_path(self):
-        if self.is_local():
-            return self.local_root + "config/api_key.json"
-        else:
-            return self.lolipop_root + "config/api_key.json"
+        base_path = "config/api_key.json"
+        return self.select_root_path() + base_path
 
     def get_keiyo_line_status_path(self):
-        if self.is_local():
-            return self.local_root + "tmp/keiyo_line_status.txt"
-        else:
-            return self.lolipop_root + "tmp/keiyo_line_status.txt"
+        base_path = "tmp/keiyo_line_status.txt"
+        return self.select_root_path() + base_path
 
     def get_message_path(self):
-        if self.is_local():
-            return self.local_root + "static_data/message.json"
-        else:
-            return self.lolipop_root + "static_data/message.json"
+        base_path = "static_data/message.json"
+        return self.select_root_path() + base_path
 
     def get_temperature_exceed_state_path(self):
-        if self.is_local():
-            return self.local_root + "tmp/temperature_exceed_state.txt"
-        else:
-            return self.lolipop_root + "tmp/temperature_exceed_state.txt"
+        base_path = "tmp/temperature_exceed_state.txt"
+        return self.select_root_path() + base_path
 
     def get_shopping_state_path(self):
-        if self.is_local():
-            return self.local_root + "tmp/shopping_goods_state.txt"
-        else:
-            return self.lolipop_root + "tmp/shopping_goods_state.txt"
+        base_path = "tmp/shopping_goods_state.txt"
+        return self.select_root_path() + base_path
 
-    def is_local(self):
-        if os.path.exists(self.local_root):
-            return True
-        else:
-            return False
-
+    def select_root_path(self):
+        for path in self.local_roots:
+            if os.path.exists(path):
+                return path
+        return self.lolipop_root
 
 if __name__ == "__main__":
     config_handler = ConfigHandler()
