ea1fb62b102c711f575a085827b6492c058fb347
igarashi339 <igarashi339@gmail.com>
2020-09-03 00:14:28 +0900

    最高気温を更新するたびにツイートするように修正

--- src/web/weather/temperature_alert.py
+++ src/web/weather/temperature_alert.py
@@ -10,28 +10,39 @@ import codecs
 if __name__ == "__main__":
     threshold_temperature = 30 # アラートを上げる気温のしきい値
 
-    # 過去にアラートをあげた日付を取得
+    # 過去にアラートをあげた日付を、気温を取得
     config_handler = ConfigHandler()
     file_path = config_handler.get_temperature_exceed_state_path()
     f = codecs.open(file_path, "r", "utf-8")
-    content = f.read()
+    content = f.read().strip()
     f.close()
+    last_alert_date = content.split(",")[0]
+    last_alert_temp = float(content.split(",")[1])
     current_time = TimeUtil.get_current_time_str().split()[0]
-    # 同日であれば処理を打ち切る
-    if current_time == content.strip():
-        exit()
 
-    # 気温がしきい値を超えていればアラートを出す
+    # 現在の気温を取得
     weather_handler = AmedasWeatherHandler()
     info = weather_handler.fetch_realtime_weather_info()
-    if info.temp > threshold_temperature:
+
+    # アラートを出すか判定
+    alert_flag = False
+    if current_time == last_alert_date:
+        # 同日に既にアラートがあがっている場合、最高気温を更新していたらアラートを出す
+        if info.temp > last_alert_temp:
+            alert_flag = True
+    else:
+        # 同日にアラートがあがっていない場合、気温がしきい値を超えていたらアラートを出す
+        if info.temp > threshold_temperature:
+            alert_flag = True
+
+    if alert_flag:
         tweet_handler = TweetHandler()
         message_str = "あつーい！いま" + str(info.temp) + "℃もあるわ～💦\n水分補給を忘れずにね❣\n"
         message_str += "#ディズニーランド #東京ディズニーシー #TDL #TDS\n"
         tweet_handler.post_tweet(message_str)
         # アラートを上げた日次を更新
         f = codecs.open(file_path, "w", "utf-8")
-        f.write(current_time)
+        f.write(current_time+","+str(info.temp))
         f.close()
 
 
